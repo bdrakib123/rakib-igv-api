dddf196bc6cd1034c55d72fe49106bf88ec1a0e6
Rakib <spyer.rakib@gmail.com>
2026-04-18 01:55:34 +0600

    Update index.js

--- index.js
+++ index.js
@@ -31,5 +31,8 @@ app.get("/", (req, res) => {
   });
 });
 
+const path = require("path");
+app.use(express.static(path.join(__dirname, "public")));
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("Server running on " + PORT));
